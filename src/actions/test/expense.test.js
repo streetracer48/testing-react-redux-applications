b804cc2741ef11e5eb894b7761f14dcb212f91c2
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-19 14:19:15 +0600

    filter actions done

--- src/actions/test/expense.test.js
+++ src/actions/test/expense.test.js
@@ -1,5 +1,4 @@
 import { addExpense, editExpense, removeExpense } from '../expenses';
-
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
--- src/actions/test/filter.test.js
+++ src/actions/test/filter.test.js
@@ -12,13 +12,48 @@ import { setStartDate,
         expect(action).toEqual({
             type:'SET_TEXT_FILTER',
             text:'sometext'
-        })
-    })
+        });
+    });
+
+    test('should genarate setText filter action object with default values', () => {
+         const action = setTextFilter();
+
+        expect(action).toEqual({
+            type:'SET_TEXT_FILTER',
+            text:''
+        });
+    });
+
 
     test('should genarte set start date action object', () => {
          const action = setStartDate(moment(0)) 
          expect(action).toEqual({
              type:'SET_START_DATE',
              startDate: moment(0)
-         })
-    })
+         });
+    });
+
+
+    test('should genarate endDate action obejct', () => {
+         const action = setEndDate(moment(0))
+
+         expect(action).toEqual({
+             type:'SET_END_DATE',
+             endDate:moment(0)
+         });
+    });
+
+test('should be genarate sort by amount action object', () => {
+    
+    expect(sortByAmount()).toEqual({
+        type:'SORT_BY_AMOUNT'
+    });
+});
+
+test('should be genarate sort by date action object', () => {
+     expect(sortByDate()).toEqual({
+         type:'SORT_BY_DATE'
+     })
+})
+
+
