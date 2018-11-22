2677349ae837158a3f3eedf32dee93f4d0d8765f
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-22 14:47:53 +0600

    sort by starting date

--- src/selector/test/selector.test.js
+++ src/selector/test/selector.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import selectorExpense from '../index'
 import expensesList from './expenseDummy'
 
@@ -14,4 +15,18 @@ test('should filter by text value', () => {
 
      const result = selectorExpense(expensesList, filters);
      expect(result).toEqual([expensesList[2], expensesList[0]])
+});
+
+test('should filter by start date ', () => {
+    const filters = {
+         text:'',
+         sortBy:'',
+         startDate:moment(0),
+         endDate:undefined
+    }
+
+    const result = selectorExpense(expensesList, filters);
+
+    expect(result).toEqual([expensesList[0], expensesList[2] ])
 })
+
